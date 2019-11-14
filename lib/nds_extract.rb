d74b2eb1691182fb9fab11f602f28e1b1edc191d
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-14 00:48:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,5 +25,7 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
+  movie_index = 0 
+  
   pp director_data[:movies][0][:worldwide_gross]
 end
